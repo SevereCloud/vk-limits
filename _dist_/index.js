5aca5d02f065b0fee08ab51d4670c1a10d0756f2
SevereCloud <SevereCloud@users.noreply.github.com>
2020-08-08 21:10:38 +0000

    deploy: 3486a736968e39083fb4b12d32b7b1adb5ec23e6

---
+++ _dist_/index.js
@@ -0,0 +1,1 @@
+import*as a from"../web_modules/react.js";import*as b from"../web_modules/react-dom.js";import{App as c}from"./App.js";b.render(a.createElement(c,null),document.getElementById("root"));
---
+++ web_modules/react.js
@@ -0,0 +1,1 @@
+export{r as default}from"./common/index-49a413ef.js";
