94a48eb9f637de22235798837066c56c38966246
SevereCloud <SevereCloud@users.noreply.github.com>
2023-03-06 10:29:01 +0000

    deploy: 72456e7c9522e7aa6bd1ade4bfdc7a653a9e476a

---
+++ _next/static/rfkBWgtKORP_LDZ3IZAOO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b8d08edd93b0c42c.css","static/chunks/pages/index-886074e5be60fe72.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
